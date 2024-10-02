63fa95472b6565bf3ba651b402c9a38d234f8a93
Sheikh Jabed <jabedbd2295@gmail.com>
2024-10-02 19:35:30 +0600

    Event Loop JS File Added and Done

--- await.js
+++ await.js
@@ -6,27 +6,27 @@ function loadUser() {
 }
 
 
-async function loadUserAsync(){
+async function loadUserAsync() {
     const res = await fetch("https://jsonplaceholder.typicode.com/users");
     const data = await res.json();
     console.log(data);
 }
 
 
-const loadUserArrow = async() =>{
+const loadUserArrow = async() => {
     const res = await fetch("https://jsonplaceholder.typicode.com/users");
     const data = await res.json();
     console.log(data);
 }
 
 
-const loadUserCatch = async() =>{
+const loadUserCatch = async() => {
     try{
         const res = await fetch("https://jsonplaceholder.typicode.com/users");
         const data = await res.json();
         console.log(data);
     }
-    catch(error){
+    catch(error) {
 
     }
 }
